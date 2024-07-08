c44cac1c29fae3c3131fda370a942ae9c0de442a
aeliyadevs <aeliyadevs@gmail.com>
2024-07-08 21:03:45 +0545

    add another horizontal rotation section

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 window.onload = function () {
-  const cards = document.querySelectorAll(".card");
-  const radius = 350;
-  const zAngle = 360 / cards.length;
-  //   console.log(cards.length);
+  // horizontal 360 rotation
+  const cards = document.querySelectorAll(".card-hori");
+  const radius = 450;
+  const yAngle = 360 / cards.length;
   cards.forEach((card, index) => {
-    console.log(index);
     card.style.transform =
       "rotateX(0deg) rotateY(" +
-      (index + 1) * zAngle +
+      (index + 1) * yAngle +
       "deg) rotateZ(0deg) translateZ(" +
       radius +
+      "px)";
+  });
+
+  // vertical 360 rotation
+  const verticals = document.querySelectorAll(".card-vert");
+  const vertRadius = 350;
+  const xAngle = 360 / verticals.length;
+  verticals.forEach((card, index) => {
+    card.style.transform =
+      "rotateX(" +
+      (index + 1) * xAngle +
+      "deg) rotateY(0deg) rotateZ(0deg) translateZ(" +
+      vertRadius +
       "px)";
   });
 };
